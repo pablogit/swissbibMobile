98c9b76888b69897549285cfe2dc803559a6e64f
Lionel Walter <lionel.walter@epfl.ch>
2011-11-01 15:11:18 +0100

    update include paths

--- include/Header.php
+++ include/Header.php
@@ -1,6 +1,6 @@
 <?php
-include('../include/Search.php');
-include('../include/DisplayItem.php');
-include('../include/MarcParser.php');
-include('../include/SruQuery.php');
+include('include/Search.php');
+include('include/DisplayItem.php');
+include('include/MarcParser.php');
+include('include/SruQuery.php');
 ?>
